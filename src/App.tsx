8c014c6288e99b1e629fac44fa5cea14575bbf77
tw <t888y@qq.com>
2019-12-06 18:07:51 +0800

    nodemon

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <img src={require('./asserts/logo.png')} alt="logo" />
+    </div>
+  );
+};
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,31 @@
-const { format } = require('url')
+const { format } = require('url');
 
-const { BrowserWindow, app } = require('electron')
-const isDev = require('electron-is-dev')
-const { resolve } = require('app-root-path')
+const { BrowserWindow, app, globalShortcut } = require('electron');
+const isDev = require('electron-is-dev');
+const { resolve } = require('app-root-path');
 
 app.on('ready', async () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    show: false,
-    titleBarStyle: 'hidden'
-  })
+    show: true,
+    titleBarStyle: 'hidden',
+    autoHideMenuBar: true
+  });
 
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show()
-    if (isDev) { mainWindow.webContents.openDevTools() }
-  })
+  globalShortcut.register('CommandOrControl+Shift+I', () => {
+    mainWindow.webContents.openDevTools();
+  });
 
-  const devPath = 'http://localhost:1234'
+  const devPath = 'http://localhost:1234';
   const prodPath = format({
     pathname: resolve('dist/renderer/index.html'),
     protocol: 'file:',
-    slashes: true,
-  })
-  const url = isDev ? devPath : prodPath
+    slashes: true
+  });
+  const url = isDev ? devPath : prodPath;
+  mainWindow.setMenu(null);
+  mainWindow.loadURL(url);
+});
 
-  mainWindow.setMenu(null)
-  mainWindow.loadURL(url)
-})
-
-app.on('window-all-closed', app.quit)
+app.on('window-all-closed', app.quit);
